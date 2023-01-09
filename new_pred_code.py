09d742478772dec13e37afa090fe4a77ccf5e8c7
priya8695 <104195293+priya8695@users.noreply.github.com>
2023-01-09 16:22:58 +0530

    Add files via upload

--- new_pred_code.py
+++ new_pred_code.py
@@ -29,7 +29,7 @@ from datetime import datetime
 from keras_self_attention import SeqSelfAttention
 from keras.layers import Dropout
 from keras.layers import InputLayer
-
+from cal_aqi import*
 
 def add_months(sourcedate, months):
     month = sourcedate.month - 1 + months
@@ -100,7 +100,7 @@ pollutants_data_test=np.asarray(df_final_test)
 #%%
 
 #Normalizing training data
-train_norm = pollutants_data_tr[:,0]
+train_norm = pollutants_data_tr
 
 #converted into array as all the methods available are for arrays and not lists
 train_norm_arr = np.asarray(train_norm)
@@ -118,7 +118,7 @@ for i in range(len(train_norm)):
         count = count +1
 print('Number of null values in train_norm = ', count)
 
-test_norm = pollutants_data_test[:,0]
+test_norm = pollutants_data_test
 test_norm_arr = np.asarray(test_norm)
 test_norm = np.reshape(test_norm_arr, (-1, 1))
 scaler = MinMaxScaler(feature_range=(0, 1))
@@ -136,52 +136,147 @@ test_norm = test_norm[test_norm != 0]
 
 #%%
 
-def split_sequence(sequence, n_steps):
-    X, y = list(), list()
-    for i in range(len(sequence)):
-        # find the end of this pattern
-        end_ix = i + n_steps
-        # check if we are beyond the sequence
-        if end_ix > len(sequence)-1:
-            break
-        # gather input and output parts of the pattern
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    return array(X),array(y)
+# def split_sequence(sequence, n_steps):
+#     X, y = list(), list()
+#     for i in range(len(sequence)):
+#         # find the end of this pattern
+#         end_ix = i + n_steps
+#         # check if we are beyond the sequence
+#         if end_ix+3 > len(sequence)-1:
+#             break
+#         # gather input and output parts of the pattern
+#         seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:end_ix+3]
+#         X.append(seq_x)
+#         y.append(seq_y)
+#     return array(X),array(y)
 
-n_steps = 3
-X_split_train, y_split_train = split_sequence(train_norm, n_steps)
-#for i in range(len(X_split_train)):
-    #print(X_split_train[i], y_split_train[i])
-n_features = 1
-X_split_train = X_split_train.reshape((X_split_train.shape[0], X_split_train.shape[1], n_features))
-for i in range(5):
-    print(X_split_train)
+# n_steps = 3
+# X_split_train, y_split_train = split_sequence(train_norm, n_steps)
+# #for i in range(len(X_split_train)):
+#     #print(X_split_train[i], y_split_train[i])
+# n_features = 1
+# X_split_train = X_split_train.reshape((X_split_train.shape[0], X_split_train.shape[1], n_features))
+# for i in range(5):
+#     print(X_split_train)
     
-X_split_test, y_split_test = split_sequence(test_norm, n_steps)
-for i in range(5):
-    print(X_split_test[i], y_split_test[i])
-n_features = 1
-X_split_test = X_split_test.reshape((X_split_test.shape[0], X_split_test.shape[1], n_features))
+# X_split_test, y_split_test = split_sequence(test_norm, n_steps)
+# for i in range(5):
+#     print(X_split_test[i], y_split_test[i])
+# n_features = 1
+# X_split_test = X_split_test.reshape((X_split_test.shape[0], X_split_test.shape[1], n_features))
 
+
+#%%
 # define model
-model = Sequential()
-model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
-model.add(Dense(1))
 
-#sgd = optimizers.SGD(lr=0.001, decay=1e-5, momentum=1.0, nesterov=False)
-sgd = optimizers.SGD(lr=0.01, decay=1e-5, momentum=0.9, nesterov=True) #good
+# model = Sequential()
+# model.add(InputLayer((3,1)))
 
-#keras.optimizers.RMSprop(learning_rate=0.01, rho=0.9)
-keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+# # model.add(LSTM(50, activation='relu', input_shape=(3, 1)))
+# # model.add(Dense(3))
+# model.add(SeqSelfAttention(
+#         attention_type=SeqSelfAttention.ATTENTION_TYPE_MUL,
+#         attention_activation='softmax',
+#         name='Attention'))
 
-hist = model.fit(X_split_train, y_split_train, validation_data=(X_split_test, y_split_test), epochs=10, verbose = 1)
 
-yhat = model.predict(X_split_test)
-for i in range(5):
-    print(yhat[i])
+# # model_lstm.add(Dense(34 ,'relu'))
+# # # model_lstm.add(Dropout(0.25))
+
+
+# # # model_lstm.add(LSTM(50))
+# # model_lstm.add(Dense(34 ,'relu'))
+# model.add(keras.layers.Bidirectional(keras.layers.LSTM(units=128,
+#                                                         return_sequences=True)))
+# model.add(Dense(34 ,'relu'))
+# # model_lstm.add(Dropout(0.25))
+
+
+# # model_lstm.add(LSTM(50))
+# model.add(Dense(34 ,'relu'))
+
+# model.add(Dense(15 ,'relu'))
+
+# model.add(Dense(1,'relu' ))
+# #sgd = optimizers.SGD(lr=0.001, decay=1e-5, momentum=1.0, nesterov=False)
+# sgd = optimizers.SGD(lr=0.01, decay=1e-5, momentum=0.9, nesterov=True) #good
+
+# #keras.optimizers.RMSprop(learning_rate=0.01, rho=0.9)
+# keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+# model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+# X_split_train=X_split_train.squeeze()
+# y_split_train=y_split_train.squeeze()
+# hist = model.fit(X_split_train, y_split_train, epochs=10, verbose = 1)
+
+# yhat = model.predict(X_split_test)
+# for i in range(5):
+#     print(yhat[i])
     
-mse = mean_squared_error(y_split_test, yhat)
-print('MSE: %.5f' % mse)
+# # mse = mean_squared_error(y_split_test, yhat)
+# mse = mean_squared_error(y_split_test, yhat.squeeze())
+# print('MSE: %.5f' % mse)
+
+
+# plt.plot(np.asarray(y_split_test[:,0]),label = 'Predict')
+# plt.plot(np.asarray(yhat[:,0]),label = 'Predict_final')
+# plt.legend()
+# plt.show()
+
+
+#%%
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+
+win_length=12
+batch_size=4
+# num_features=features.shape[1]
+X_train=train_norm_arr
+y_train=train_norm_arr
+X_test=test_norm_arr
+y_test=test_norm_arr
+
+train_generator = TimeseriesGenerator(X_train, y_train, length=win_length, sampling_rate=1, batch_size=batch_size)
+# val_generator = TimeseriesGenerator(X_val, y_val, length=win_length, sampling_rate=1, batch_size=batch_size)
+test_generator = TimeseriesGenerator(X_test, y_test, length=win_length, sampling_rate=1, batch_size=batch_size)
+model_LSTM = tf.keras.Sequential([
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(100,  return_sequences=True), input_shape=(win_length, 7)),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(100, return_sequences=False)),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(7)
+])
+
+model_LSTM.compile(optimizer=tf.keras.optimizers.Nadam(learning_rate=0.001), loss=tf.losses.MeanSquaredLogarithmicError())
+
+tf.keras.utils.plot_model(model=model_LSTM, show_shapes=True)
+lr_monitor = tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=3, factor=0.5, cooldown=1)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=15, restore_best_weights=True)
+
+with tf.device('/GPU:0'):
+    prepared_model = model_LSTM.fit(train_generator, 
+                                   
+                                    epochs=200, 
+                                    shuffle=False,  
+                                    callbacks=[lr_monitor, early_stopping])
+
+
+predictions=model_LSTM.predict(test_generator)
+
+# plt.plot(np.asarray(predictions[:,3]),label = 'Predict')
+# plt.plot(np.asarray(test_norm_arr[12:248,3]),label = 'Predict_final')
+# plt.legend()
+# plt.show()
+
+pred_final=[]
+for i in range(len(predictions)):
+    temp=get_AQI(predictions[i])
+    pred_final.append(temp[0])
+
+test=pollutants_aqi_test_final.to_numpy()
+pred_final=np.asarray(pred_final,dtype=np.float32)
+plt.plot(pred_final,label = 'Predict')
+plt.plot(test[win_length:],label = 'Predict_final')
+plt.legend()
+plt.show()
+    
+
+#%%
